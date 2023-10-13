f5520a56b3d40189b2a45156d547547a73a1f033
Jean Janssen <jean.janssen@polymtl.ca>
2023-10-13 15:39:07 -0400

    Ajout du code et correction

--- TP2/TP2_tests.py
+++ TP2/TP2_tests.py
@@ -6,35 +6,34 @@ import os
 import glymur
 
 # Load an image
-image_originale = cv2.imread(r"c:\Users\Jean Janssen\Desktop\keyssh\INF8770\TP2\RGB.jpg")
-original_image = glymur.Jp2k(r"c:\Users\Jean Janssen\Desktop\keyssh\INF8770\TP2\RGB.jpg")
+image_originale = cv2.imread(r"/home/jeanjanssen/INF8770/INF8770/TP2/RGB.jpg")
+test_image = cv2.imread(r"/home/jeanjanssen/INF8770/INF8770/TP2/RGB.jp2")
+jp2_image_originale = glymur.Jp2k(r"/home/jeanjanssen/INF8770/INF8770/TP2/RGB.jp2")
 
 # Compression JPEG
+compression_quality = 90  # Adjust the quality value as needed
 jpeg_filename = "image_compressed.jpg"
-_, jpeg_data = cv2.imencode(".jpg", image_originale, [cv2.IMWRITE_JPEG_QUALITY, 90])
-with open(jpeg_filename, "wb") as f:
-    f.write(jpeg_data)
+cv2.imwrite(jpeg_filename, image_originale, [int(cv2.IMWRITE_JPEG_QUALITY), compression_quality])
+
 
 # Create a JP2 image with compression options
-compression_options = {'rate': 20}  # Adjust the rate for the desired compression level
-compressed_image = glymur.Jp2k('jp2k_filename.jp2', data=original_image, **compression_options)
-
-# Use Glymur to read the JPEG2000 image
-image_jp2k = glymur.Jp2k(jp2k_filename)
+# compression_options = {'rate': 20}  # Adjust the rate for the desired compression level
+jp2k_filename = "output_image.jp2"
+compression_ratios  = [5.0]
+compressed_image = glymur.Jp2k(jp2k_filename, data=jp2_image_originale, cratios=compression_ratios, colorspace='rgb')
 
 # Calculate the PSNR (Peak Signal-to-Noise Ratio)
 psnr_jpeg = psnr(image_originale, cv2.imread(jpeg_filename))
-psnr_jpeg2000 = psnr(image_originale, cv2.imread(jp2k_filename))
+psnr_jpeg2000 = psnr(test_image, cv2.imread(jp2k_filename))
 
 # Calculate the SSIM (Structural Similarity Index)
 ssim_jpeg = ssim(image_originale, cv2.imread(jpeg_filename), win_size=3)
-ssim_jpeg2000 = ssim(image_originale, cv2.imread(jp2k_filename), win_size=3)
+ssim_jpeg2000 = ssim(test_image, cv2.imread(jp2k_filename), win_size=3)
 
 # Calculate file sizes and compression ratios
-size_jpeg = os.path.getsize(jpeg_filename)
-size_jpeg2000 = os.path.getsize(jp2k_filename)
-compression_ratio_jpeg = os.path.getsize(r"c:\Users\Jean Janssen\Desktop\keyssh\INF8770\TP2\RGB.jpg") / size_jpeg
-compression_ratio_jpeg2000 = os.path.getsize(r"c:\Users\Jean Janssen\Desktop\keyssh\INF8770\TP2\RGB.jpg") / size_jpeg2000
+compression_ratio_jpeg =  os.path.getsize(r"/home/jeanjanssen/INF8770/INF8770/TP2/RGB.jpg")/os.path.getsize(jpeg_filename)
+compression_ratio_jpeg2000 = os.path.getsize(r"/home/jeanjanssen/INF8770/INF8770/TP2/RGB.jp2")/os.path.getsize(jp2k_filename) 
+
 
 # Display or print the results
 print("PSNR JPEG:", psnr_jpeg)
